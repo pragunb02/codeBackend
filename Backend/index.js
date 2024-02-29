ca3114c470e5539e6491c39688287e24d06445f2
Pragun Bhutani <pragun.bhutani897@gmail.com>
2024-03-01 03:49:57 +0530

    intent

--- Backend/index.js
+++ Backend/index.js
@@ -1,48 +1,49 @@
 // const express=require('express');  SYNCHRONOUS JS
-import express from 'express';
+import express from "express";
 
-const app=express();
+const app = express();
 
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('dist'))  // dist folder vite build
+app.use(express.static("dist")); // dist folder vite build
 
 // app.get('/',(req,res)          =>{
 //      res.send('Server is Ready (home)');
 // });
 // GET A LIST OF 5 JOKES
-app.get('/api/jokes',(req,res)=>{   // like api (api array , json object)
-    // array of jokes each have objects
-    const jokes=[
-        {
-            id:1,
-            title:'A first Joke',
-            content:'This is a first joke'
-        },
-        {
-            id:2,
-            title:'A second Joke',
-            content:'This is a second joke'
-        },
-        {
-            id:3,
-            title:'A third Joke',
-            content:'This is a third joke'
-        },
-        {
-            id:4,
-            title:'A fourth Joke',
-            content:'This is a fourth joke'
-        },
-        {
-            id:5,
-            title:'A fifth Joke',
-            content:'This is a fifth joke'
-        }
-    ];
-    res.send(jokes);
+app.get("/api/jokes", (req, res) => {
+  // like api (api array , json object)
+  // array of jokes each have objects
+  const jokes = [
+    {
+      id: 1,
+      title: "A first Joke",
+      content: "This is a first joke",
+    },
+    {
+      id: 2,
+      title: "A second Joke",
+      content: "This is a second joke",
+    },
+    {
+      id: 3,
+      title: "A third Joke",
+      content: "This is a third joke",
+    },
+    {
+      id: 4,
+      title: "A fourth Joke",
+      content: "This is a fourth joke",
+    },
+    {
+      id: 5,
+      title: "A fifth Joke",
+      content: "This is a fifth joke",
+    },
+  ];
+  res.send(jokes);
 });
-app.listen(port,()=>{
-    console.log("jdk");
-    console.log(`Serve at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log("jdk");
+  console.log(`Serve at http://localhost:${port}`);
 });
